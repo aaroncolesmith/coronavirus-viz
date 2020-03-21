16f4f92149d3ec0e34a00249388db354af998001
Aaron <acs2210@gmail.com>
2020-03-21 08:46:40 -0700

    make it better

--- coronavirus-viz.py
+++ coronavirus-viz.py
@@ -18,7 +18,7 @@ def _max_width_():
 
 _max_width_()
 
-st.title("Coronavirus Viz")
+st.title("Coronavirus-Viz")
 st.markdown("Feel free to explore the data. Click on an item in the legend to filter it out -- double-click an item to filter down to just that item. Or click and drag to filter the view so that you only see the range you are looking for.")
 st.write("If you have any feedback or questions, feel free to get at me on the [Twitter] (https://www.twitter.com/aaroncolesmith) machine")
 
