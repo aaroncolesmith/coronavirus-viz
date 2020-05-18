38cd903dcfd319961f6d06b791354f9c21727946
Aaron <acs2210@gmail.com>
2020-05-18 07:50:04 -0700

    make better

--- coronavirus-viz.py
+++ coronavirus-viz.py
@@ -58,7 +58,6 @@ def load_data_us():
 
     return df_us
 
-@st.cache
 def load_data_global():
     confirmed_all = pd.read_csv('./csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv')
     confirmed_all=confirmed_all.set_index(['Province/State','Country/Region','Lat','Long']).stack().reset_index()
